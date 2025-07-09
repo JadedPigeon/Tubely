6047dbe558df364cd1913cbc2887ae1c8d73fe2e
JadedPigeon <JadedPigeon137@gmail.com>
2025-07-09 18:46:40 -0400

    server side caching

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -60,7 +60,6 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		respondWithError(w, http.StatusBadRequest, "Only JPEG and PNG thumbnails are allowed", nil)
 		return
 	}
-	//I won't comment again
 
 	exts, _ := mime.ExtensionsByType(mediaType)
 	ext := ""
